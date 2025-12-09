75132d6524630ca712c1a63ed305cdac1da2a43d
HelioJr <heliojr548@gmail.com>
2025-12-09 14:48:55 -0300

    Recebendo dados: TextInput + useState + onChangeText dinâmico

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,12 +1,41 @@
 import { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 export default function App() {
+	const [name, setName] = useState('');
+
+	function getName(text) {
+		text.length > 0 ? setName(`Bem vindo ${text}`) : setName('');
+	}
+
 	return (
-		<View style={{ paddingTop: 40, flex: 1, flexDirection: 'column', alignItems: 'stretch' }}>
-			<View style={{ width: 50, height: 50, backgroundColor: 'green' }}></View>
-			<View style={{ height: 50, backgroundColor: 'blue' }}></View>
-			<View style={{ height: 50, backgroundColor: 'red' }}></View>
+		<View style={styles.container}>
+			<TextInput
+				style={styles.input}
+				placeholder="Digite seu nome"
+				onChangeText={(text) => getName(text)}
+			/>
+
+			<Text style={styles.text}>{name}</Text>
 		</View>
-	)
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		paddingTop: 40,
+		flex: 1,
+	},
+	input: {
+		height: 45,
+		borderWidth: 1,
+		margin: 10,
+		padding: 10,
+		fontSize: 20,
+		borderRadius: 10,
+	},
+	text: {
+		textAlign: 'center',
+		fontSize: 25,
+	},
+});
