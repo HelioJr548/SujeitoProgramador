9d03f8b770621645e5212e45f89b9dbb4a3eacaa
HelioJr <heliojr548@gmail.com>
2025-12-09 10:57:59 -0300

    Props: style

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,11 +1,21 @@
 import { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export default class App extends Component {
 	render() {
 		return (
 			<View>
 				<Text>Hello, World!</Text>
+				<Text style={{ color: '#ff0000', fontSize: 25, margin: 15 }}>
+					Olá, Mundo!
+				</Text>
+
+				<Image
+					source={{
+						uri: 'http://sujeitoprogramador.com/reactlogo.png',
+					}}
+					style={{ width: 200, height: 200 }}
+				/>
 			</View>
 		);
 	}
