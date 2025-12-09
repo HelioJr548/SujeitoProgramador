d2d85f6321c27709e5e49d1146395569dfa8d5f2
HelioJr <heliojr548@gmail.com>
2025-12-09 14:09:47 -0300

    Flexbox e alinhamentos

--- meuapp/App.js
+++ meuapp/App.js
@@ -3,10 +3,15 @@ import { View, Text } from 'react-native';
 
 export default function App() {
 	return (
-		<View style={{ paddingTop: 40, backgroundColor: 'blue', flex: 1 }}>
-			<View style={{ height: 65, backgroundColor: '#333' }}></View>
-			<View style={{ flex: 1, backgroundColor: '#ddd', }}></View>
-			<View style={{ height: 65, backgroundColor: '#333', }}></View>
+		<View style={{
+			paddingTop: 40, flex: 1, 
+			flexDirection: 'collumn',
+			justifyContent: 'flex-start',
+			alignItems: 'flex-end'
+		}}>
+			<View style={{ height: 50, width: 50, backgroundColor: '#333' }}></View>
+			<View style={{ height: 50, width: 50, backgroundColor: 'red' }}></View>
+			<View style={{ height: 50, width: 50, backgroundColor: 'green' }}></View>
 		</View>
 	);
 };
