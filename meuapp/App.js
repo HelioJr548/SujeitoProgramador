5f9c89badb09d6f9180aae6e78e3f4764d16a96f
HelioJr <heliojr548@gmail.com>
2025-12-09 12:11:27 -0300

    useState + Button onPress atualizando múltiplos states (2° opção)

--- meuapp/App.js
+++ meuapp/App.js
@@ -5,14 +5,15 @@ export default function App() {
 	const [name, setName] = useState('Helio');
 	const [age, setAge] = useState(22);
 
-	function entrar(name, age) {
-		setName(name);
-		setAge(age);
-	}
-
 	return (
 		<View style={{ marginTop: 25 }}>
-			<Button title="Mudar Nome" onPress={() => entrar('Helio Jr', 23)} />
+			<Button
+				title="Mudar Nome"
+				onPress={() => {
+					setName('Helio Jr');
+					setAge(23);
+				}}
+			/>
 
 			<Text style={{ fontSize: 20 }}>{name}</Text>
 			<Text style={{ fontSize: 18 }}>{age}</Text>
