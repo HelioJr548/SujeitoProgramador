81b1daa839240d1e039f551129e7b4aadf238a78
HelioJr <heliojr548@gmail.com>
2025-12-09 13:34:31 -0300

    Aplicando grupo de estilo

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,22 +1,32 @@
 import { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
-	const [name, setName] = useState('Helio');
-	const [age, setAge] = useState(22);
-
 	return (
-		<View style={{ marginTop: 25 }}>
-			<Button
-				title="Mudar Nome"
-				onPress={() => {
-					setName('Helio Jr');
-					setAge(23);
-				}}
-			/>
+		<View style={styles.area}>
+			
 
-			<Text style={{ fontSize: 20 }}>{name}</Text>
-			<Text style={{ fontSize: 18 }}>{age}</Text>
+			<Text style={[styles.title, styles.centerText]}>Helio</Text>
+			<Text style={styles.title}>Sujeito Programador</Text>
+			<Text style={styles.subTitle}>Sou um texto 3</Text>
 		</View>
 	);
-}
+};
+
+const styles = StyleSheet.create({
+	area:{
+		marginTop: 50
+	},
+	title: {
+		fontSize: 20,
+		color: '#ff0000'
+	},
+	subTitle: {
+		color: '#00FF00',
+		fontSize: 17,
+		marginTop: 15,
+	},
+	centerText: {
+		textAlign: 'center'
+	}
+}) 
