a0628c157ed3f3ac3d62f64277109027abf62e01
HelioJr <heliojr548@gmail.com>
2025-12-09 10:21:02 -0300

    Criando componente em forma de Classe

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,9 +1,12 @@
+import { Component } from 'react';
 import { View, Text } from 'react-native';
 
-export default function App() {
-	return (
-		<View>
-			<Text>Hello, World!</Text>
-		</View>
-	);
+export default class App extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Hello, World!</Text>
+			</View>
+		);
+	}
 }
