e6bc2043e5f68e30b4b19999b4a8cdf1752ef1ce
HelioJr <heliojr548@gmail.com>
2025-12-12 16:00:47 -0300

    ScrollView Horizontal

--- meuapp/App.js
+++ meuapp/App.js
@@ -3,7 +3,10 @@ import { View, StyleSheet, ScrollView } from 'react-native';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<ScrollView showsVerticalScrollIndicator={false}>
+			<ScrollView
+				horizontal={true}
+				showsHorizontalScrollIndicator={false}
+			>
 				<View style={styles.box1} />
 				<View style={styles.box2} />
 				<View style={styles.box3} />
@@ -20,22 +23,27 @@ const styles = StyleSheet.create({
 		flex: 1,
 	},
 	box1: {
+		width: 150,
 		height: 250,
 		backgroundColor: 'blue',
 	},
 	box2: {
+		width: 150,
 		height: 250,
 		backgroundColor: 'red',
 	},
 	box3: {
+		width: 150,
 		height: 250,
 		backgroundColor: 'green',
 	},
 	box4: {
+		width: 150,
 		height: 250,
 		backgroundColor: 'purple',
 	},
 	box5: {
+		width: 150,
 		height: 250,
 		backgroundColor: 'orange',
 	},
