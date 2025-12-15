bca2eeaa033aed5391eb4594c4a5b64dd47c515c
HelioJr <heliojr548@gmail.com>
2025-12-15 13:05:53 -0300

    Slider

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,37 +1,24 @@
-import { Picker } from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
 import { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 export default function App() {
-	const [carroSelecionado, setCarroSelecionado] = useState(0);
-	const [carros, setCarros] = useState([
-		{ key: 1, nome: 'Ford', valor: 62.0 },
-		{ key: 2, nome: 'Chevrolet', valor: 58.0 },
-		{ key: 3, nome: 'Honda', valor: 70.0 },
-		{ key: 4, nome: 'Hyundai', valor: 55.0 },
-		{ key: 5, nome: 'Volkswagen', valor: 68.0 },
-	]);
-
-	let carrosItem = carros.map((v, k) => {
-		return <Picker.Item key={k} value={k} label={v.nome} />;
-	});
+	const [value, setValue] = useState(50);
 
 	return (
 		<View style={styles.container}>
-			<Picker
-				selectedValue={carroSelecionado}
-				onValueChange={(itemValue, itemIndex) =>
-					setCarroSelecionado(itemValue)
-				}
-			>
-				{carrosItem}
-			</Picker>
+			<Slider
+				minimumValue={0}
+				maximumValue={100}
+				value={value}
+				onValueChange={(selectedValue) => setValue(selectedValue)}
+				minimumTrackTintColor="hsla(96, 34%, 50%, 1.00)" 
+				maximumTrackTintColor="hsla(0, 100%, 73%, 1.00)"
+				thumbTintColor="hsla(56, 51%, 73%, 1.00)"
+			/>
 
-			<Text style={styles.carros}>
-				Carro selecionado: {carros[carroSelecionado].nome}
-			</Text>
-			<Text style={styles.carros}>
-				Valor: {carros[carroSelecionado].valor.toFixed(3)}
+			<Text style={{ textAlign: 'center', marginTop: 20, fontSize: 20 }}>
+				Selected Value: {value.toFixed(0)}
 			</Text>
 		</View>
 	);
@@ -42,10 +29,5 @@ const styles = StyleSheet.create({
 		paddingTop: 40,
 		flex: 1,
 		backgroundColor: '#f5f5f5',
-	},
-	carros: {
-		fontSize: 20,
-		textAlign: 'center',
-		marginTop: 20,
 	},
 });
