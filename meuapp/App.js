6ea2207c9365ba3cd3f2d027f37c2df36b80fc66
HelioJr <heliojr548@gmail.com>
2025-12-16 17:40:47 -0300

    Realizando dispatch para limpar pilha de paginas (ultima pagina para a primeira)

--- meuapp/App.js
+++ meuapp/App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/pages/Home';
 import About from './src/pages/About';
+import Contact from './src/pages/Contact';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,12 +17,23 @@ const App = () => {
 						title: 'Tela inicio',
 						headerStyle: { backgroundColor: '#121212' },
 						headerTintColor: '#fff',
-						headerShown: false
+						headerShown: false,
 					}}
 				/>
-				<Stack.Screen name="About" component={About} options={{
-					title: 'Sobre'
-				}}/>
+				<Stack.Screen
+					name="About"
+					component={About}
+					options={{
+						title: 'Sobre',
+					}}
+				/>
+				<Stack.Screen
+					name="Contact"
+					component={Contact}
+					options={{
+						title: 'Contato',
+					}}
+				/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
--- meuapp/src/pages/About/index.js
+++ meuapp/src/pages/About/index.js
@@ -1,14 +1,26 @@
-import { useRoute } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { useLayoutEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 const About = () => {
 	const route = useRoute();
+	const navigation = useNavigation();
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			title:
+				route.params?.nome === '' ? 'Pagina Sobre' : route.params?.nome,
+		});
+	}, [navigation]);
 
 	return (
 		<View style={styles.container}>
 			<Text>Tela About</Text>
 			<Text>{route.params?.nome}</Text>
 			<Text>{route.params?.email}</Text>
+
+			<Button title="IR PARA CONTATO" onPress={() => navigation.navigate('Contact')}/>
+			<Button title="Voltar Tela" onPress={() => navigation.goBack()}/>
 		</View>
 	);
 };
