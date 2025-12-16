20952c805d7eddc8bda1c7024d13021bab9565e3
HelioJr <heliojr548@gmail.com>
2025-12-16 17:25:51 -0300

    Usando hook useRoute()

--- meuapp/src/pages/About/index.js
+++ meuapp/src/pages/About/index.js
@@ -1,21 +1,24 @@
+import { useRoute } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 
-const About = ({route}) => {
-    return (
-        <View style={styles.container}>
-            <Text>Tela About</Text>
-            <Text>{route.params?.nome}</Text>
-            <Text>{route.params?.email}</Text>
-        </View>
-    );
+const About = () => {
+	const route = useRoute();
+
+	return (
+		<View style={styles.container}>
+			<Text>Tela About</Text>
+			<Text>{route.params?.nome}</Text>
+			<Text>{route.params?.email}</Text>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 });
 
 export default About;
