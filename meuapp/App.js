a4a2b9937c205019f63d18172d4c1ae6a473cbee
HelioJr <heliojr548@gmail.com>
2025-12-16 13:53:07 -0300

    Utilizando useEffect (desmontando componente)

--- meuapp/App.js
+++ meuapp/App.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text, Switch, Button } from 'react-native';
 
 export default function App() {
 	const [contador, setContador] = useState(0);
+	const [renderizado, setRenderizado] = useState(true);
 
 	useEffect(() => {
 		console.log('MONTOU');
@@ -13,9 +14,26 @@ export default function App() {
 			<Button title="➕" onPress={() => setContador(contador + 1)} />
 			<Text style={{ fontSize: 30 }}>{contador}</Text>
 			<Button title="➖" onPress={() => setContador(contador - 1)} />
+
+			<Button
+				title="MOSTRAR NOME"
+				onPress={() => setRenderizado(!renderizado)}
+			/>
+
+			{/* caso renderizado = TRUE renderiza <Nome/> */}
+			{renderizado && <Nome />}
 		</View>
 	);
 }
+
+const Nome = () => {
+	useEffect(() => {
+		console.log('COMPONENTE NOME FOI MONTADO NA TELA!');
+
+		return () => console.log('COMPONENTE DESMONTADO'); // chamado ao componente ser desmontado (sair da tela)
+	}, []);
+	return <Text>Helio</Text>;
+};
 
 const styles = StyleSheet.create({
 	container: {
