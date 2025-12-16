f3099631edd5665d02d9a9d5131cb2debe52d9da
HelioJr <heliojr548@gmail.com>
2025-12-16 16:12:07 -0300

    Utilizando Icons (Expo)

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,25 +1,22 @@
 import { useState } from 'react';
-import { Button, Modal, StyleSheet, Text, View } from 'react-native';
-import Detalhes from './src/Detalhes';
+import { StyleSheet, View } from 'react-native';
+
+// https://icons.expo.fyi/Index --> Site com todos os icons para o Expo
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function App() {
-	const [modalVisible, setModalVisible] = useState(false);
-
-	const abrirModal = () => setModalVisible(true);
-
-	const fecharModal = () => setModalVisible(false);
-
 	return (
 		<View style={styles.container}>
-			<Button title="Acessar" onPress={abrirModal} />
+			<Ionicons name="checkmark-circle" size={45} color="green" />
 
-			<Modal
-				visible={modalVisible}
-				animationType="slide"
-				transparent={true}
+			<FontAwesome.Button // https://docs.expo.dev/guides/icons/#button-component
+				name="facebook"
+				backgroundColor="hsla(221, 44%, 41%, 1.00)"
+				// onPress={loginWithFacebook}
 			>
-				<Detalhes fechar={fecharModal} />
-			</Modal>
+				Login with Facebook
+			</FontAwesome.Button>
 		</View>
 	);
 }
