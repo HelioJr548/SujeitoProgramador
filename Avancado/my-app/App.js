041d237f777da9d2431a05e4a1dea530fa85992d
HelioJr <heliojr548@gmail.com>
2025-12-18 11:33:41 -0300

    Requisições HTTP

---
+++ Avancado/my-app/App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import api from './src/services/api';
+import Filmes from './src/components/Filmes';
+
+export default function App() {
+	const [filmes, setFilmes] = useState([]);
+
+	useEffect(() => {
+		const loadFilmes = async () => {
+			const { data } = await api.get('r-api/?api=filmes');
+			// console.log(data);
+			setFilmes(data);
+		};
+
+		loadFilmes();
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<FlatList
+				data={filmes}
+				keyExtractor={(item) => item.id}
+				renderItem={({ item }) => <Filmes data={item} />}
+			/>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
