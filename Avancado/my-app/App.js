d204e30b70d15ea1a7f13c28fec26751d1a0c682
HelioJr <heliojr548@gmail.com>
2025-12-18 13:49:10 -0300

    Criando Loading

--- Avancado/my-app/App.js
+++ Avancado/my-app/App.js
@@ -1,32 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import {
+	ActivityIndicator,
+	FlatList,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
 import api from './src/services/api';
 import Filmes from './src/components/Filmes';
 
 export default function App() {
 	const [filmes, setFilmes] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const loadFilmes = async () => {
 			const { data } = await api.get('r-api/?api=filmes');
-			// console.log(data);
 			setFilmes(data);
+			setLoading(false);
 		};
 
 		loadFilmes();
 	}, []);
 
-	return (
-		<View style={styles.container}>
-			<FlatList
-				data={filmes}
-				keyExtractor={(item) => item.id}
-				renderItem={({ item }) => <Filmes data={item} />}
-			/>
-			<StatusBar style="auto" />
-		</View>
-	);
+	if (loading) {
+		return (
+			<View
+				style={{
+					alignItems: 'center',
+					justifyContent: 'center',
+					flex: 1,
+				}}
+			>
+				<ActivityIndicator color="#121212" size={45} />
+			</View>
+		);
+	} else {
+		return (
+			<View style={styles.container}>
+				<FlatList
+					data={filmes}
+					keyExtractor={(item) => item.id}
+					renderItem={({ item }) => <Filmes data={item} />}
+				/>
+				<StatusBar style="auto" />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
