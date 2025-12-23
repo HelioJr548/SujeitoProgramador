f40e450c13ed313704114c937f5b6e735207bbee
HelioJr <heliojr548@gmail.com>
2025-12-23 13:54:49 -0300

    Funções: Arrow functions

--- Avancado/Typescript/dist/functions/funcoes.js
+++ Avancado/Typescript/dist/functions/funcoes.js
@@ -1,21 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function login(username) {
-    let message = `Bem vindo, ${username}`;
-    console.log(message);
-    return username;
-}
-const usernameLogin = login('Helio');
-console.log(usernameLogin);
-let n1 = 10;
-let n2 = 25;
-function soma(valor1, valor2) {
-    let soma = valor1 + valor2;
-    if (soma > 20) {
-        return 'SOMA MAIOR QUE 20';
-    }
-    else {
-        return 'SOMA MENOR QUE 20';
-    }
-}
-console.log(soma(n1, n2));
+const retornoApi = (url) => {
+    console.log(`URL DA API: ${url}`);
+};
+retornoApi('https://...com.br');
--- Avancado/Typescript/src/functions/funcoes.ts
+++ Avancado/Typescript/src/functions/funcoes.ts
@@ -1,21 +1,27 @@
-function login(username: string): boolean | string {
-	let message = `Bem vindo, ${username}`;
-	console.log(message);
+// function login(username: string): boolean | string {
+// 	let message = `Bem vindo, ${username}`;
+// 	console.log(message);
 
-	return username;
-}
+// 	return username;
+// }
 
-const usernameLogin = login('Helio');
-console.log(usernameLogin);
+// const usernameLogin = login('Helio');
+// console.log(usernameLogin);
 
-let n1: number = 10;
-let n2: number = 25;
-function soma(valor1: number, valor2: number): string {
-	let soma = valor1 + valor2;
-	if (soma > 20) {
-		return 'SOMA MAIOR QUE 20';
-	} else {
-		return 'SOMA MENOR QUE 20';
-	}
-}
-console.log(soma(n1, n2));
+// let n1: number = 10;
+// let n2: number = 25;
+// function soma(valor1: number, valor2: number): string {
+// 	let soma = valor1 + valor2;
+// 	if (soma > 20) {
+// 		return 'SOMA MAIOR QUE 20';
+// 	} else {
+// 		return 'SOMA MENOR QUE 20';
+// 	}
+// }
+// console.log(soma(n1, n2));
+
+// ARROW FUNCTIONS
+const retornoApi = (url: string): void => {
+	console.log(`URL DA API: ${url}`);
+};
+retornoApi('https://...com.br');
