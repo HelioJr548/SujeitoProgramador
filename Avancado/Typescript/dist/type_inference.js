006d81c546bc72c33bdca72a9d13ea0ca1b28899
HelioJr <heliojr548@gmail.com>
2025-12-23 10:29:00 -0300

    Type inference

---
+++ Avancado/Typescript/dist/type_inference.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let curso = 'Typescript';
+let tecnologias = ['PHP', 'React JS'];
+tecnologias.push('React Native');
+tecnologias.push(2021);
+console.log(tecnologias);
