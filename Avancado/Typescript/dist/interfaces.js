fc05454c447629d385d22fb2ac175a1550584b17
HelioJr <heliojr548@gmail.com>
2025-12-23 15:31:02 -0300

    Interfaces: readonly

--- Avancado/Typescript/dist/interfaces.js
+++ Avancado/Typescript/dist/interfaces.js
@@ -1,19 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function mostraPromocao(preco) {
-    console.log(`Promoção no curso por apenas: R$ ${preco}`);
-}
-const novoCurso = {
+let produto1 = {
     id: '1',
-    nome: 'Curso TS',
-    preco: 750,
-    promocao: mostraPromocao,
+    nome: 'Tenis',
+    descricao: 'Tenis bom',
 };
-console.log(novoCurso);
-console.log(novoCurso.promocao(350));
-let somaNumeros = (valor1, valor2) => {
-    console.log(`Resultado: ${valor1 + valor2}`);
-    return valor1 + valor2;
-};
-const resultado = somaNumeros(15, 10);
-console.log(`Resultado da variavel: ${resultado}`);
+produto1.id = '123';
+console.log(produto1);
--- Avancado/Typescript/src/interfaces.ts
+++ Avancado/Typescript/src/interfaces.ts
@@ -38,38 +38,54 @@
 // }
 // novoUser({ email: 'ana@', status: false });
 
-// Interfaces com funções
-interface ICursoProps {
-	id: string;
+// // Interfaces com funções
+// interface ICursoProps {
+// 	id: string;
+// 	nome: string;
+// 	preco: number;
+
+// 	// definir apenas a funçao e o que ela deve esperar e retornar
+// 	promocao: (preco: number) => void;
+// }
+
+// function mostraPromocao(preco: number) {
+// 	console.log(`Promoção no curso por apenas: R$ ${preco}`);
+// }
+
+// const novoCurso: ICursoProps = {
+// 	id: '1',
+// 	nome: 'Curso TS',
+// 	preco: 750,
+// 	promocao: mostraPromocao,
+// };
+
+// console.log(novoCurso);
+// console.log(novoCurso.promocao(350));
+
+// interface ISomaProps {
+// 	(valor1: number, valor2: number): number;
+// }
+// let somaNumeros: ISomaProps = (valor1: number, valor2: number): number => {
+// 	console.log(`Resultado: ${valor1 + valor2}`);
+// 	return valor1 + valor2;
+// };
+
+// const resultado = somaNumeros(15, 10);
+
+// console.log(`Resultado da variavel: ${resultado}`);
+
+// ReadOnly
+interface IProdutoProps {
+	readonly id: string;
 	nome: string;
-	preco: number;
-
-	// definir apenas a funçao e o que ela deve esperar e retornar
-	promocao: (preco: number) => void;
+	descricao: string;
 }
 
-function mostraPromocao(preco: number) {
-	console.log(`Promoção no curso por apenas: R$ ${preco}`);
-}
-
-const novoCurso: ICursoProps = {
+let produto1: IProdutoProps = {
 	id: '1',
-	nome: 'Curso TS',
-	preco: 750,
-	promocao: mostraPromocao,
+	nome: 'Tenis',
+	descricao: 'Tenis bom',
 };
 
-console.log(novoCurso);
-console.log(novoCurso.promocao(350));
-
-interface ISomaProps {
-	(valor1: number, valor2: number): number;
-}
-let somaNumeros: ISomaProps = (valor1: number, valor2: number): number => {
-	console.log(`Resultado: ${valor1 + valor2}`);
-	return valor1 + valor2;
-};
-
-const resultado = somaNumeros(15, 10);
-
-console.log(`Resultado da variavel: ${resultado}`);
+produto1.id = '123';
+console.log(produto1);
