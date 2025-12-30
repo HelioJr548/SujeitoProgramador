bb841f7ac167d01ee911b3a4455bd02a7b265cdc
HelioJr <heliojr548@gmail.com>
2025-12-30 09:24:40 -0300

    Cadastrando no banco

--- Avancado/Firebase/App.js
+++ Avancado/Firebase/App.js
@@ -1,25 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { db } from './src/firebaseConnection';
 import { useEffect, useState } from 'react';
-import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot, setDoc } from 'firebase/firestore';
 
 export default function App() {
 	const [nome, setNome] = useState('Carregando...');
 
-	useEffect(() => {
-		async function getDados() {
-			onSnapshot(doc(db, 'users', '1'), (doc) => {
-				setNome(doc.data()?.nome);
-			});
-		}
+	// useEffect(() => {
+	// 	async function getDados() {
+	// 		onSnapshot(doc(db, 'users', '1'), (doc) => {
+	// 			setNome(doc.data()?.nome);
+	// 		});
+	// 	}
 
-		getDados();
-	}, []);
+	// 	getDados();
+	// }, []);
+
+	async function handleRegister() {
+		await setDoc(doc(db, 'users', '3'), {
+			nome: 'Junior',
+			idade: '23',
+			cargo: 'Backend',
+		});
+	}
 
 	return (
 		<View style={styles.container}>
 			<Text style={{ fontSize: 24 }}>Nome: {nome}</Text>
+
+			<Pressable style={styles.button} onPress={handleRegister}>
+				<Text style={styles.buttonText}>Adicionar</Text>
+			</Pressable>
+
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -31,5 +44,12 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	button: {
+		backgroundColor: '#000',
+	},
+	buttonText: {
+		padding: 8,
+		color: '#fff',
 	},
 });
