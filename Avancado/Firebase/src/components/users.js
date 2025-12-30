1a7c9def74e3aa12d3e759f21addbdaa2520053c
HelioJr <heliojr548@gmail.com>
2025-12-30 16:34:15 -0300

    Deletando item do banco

--- Avancado/Firebase/src/components/users.js
+++ Avancado/Firebase/src/components/users.js
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { db } from '../firebaseConnection';
 
 export function UsersList({ data }) {
+	async function handleDeleteItem() {
+		const docRef = doc(db, 'users', data.id);
+		await deleteDoc(docRef);
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.item}>Nome: {data.nome}</Text>
 			<Text style={styles.item}>Idade: {data.idade}</Text>
 			<Text style={styles.item}>Cargo: {data.cargo}</Text>
+			<Pressable style={styles.button} onPress={handleDeleteItem}>
+				<Text style={styles.buttonText}>Deletar usuário</Text>
+			</Pressable>
 		</View>
 	);
 }
@@ -20,5 +30,16 @@ const styles = StyleSheet.create({
 	item: {
 		color: '#000',
 		fontSize: 16,
+	},
+	button: {
+		backgroundColor: '#b3261e',
+		alignSelf: 'flex-start',
+		padding: 4,
+		borderRadius: 4,
+		marginTop: 16,
+	},
+	buttonText: {
+		color: '#fff',
+		paddingHorizontal: 8,
 	},
 });
