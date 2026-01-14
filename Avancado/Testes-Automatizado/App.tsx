fd3162086cf88d007af535b2142e271c5f4bb415
HelioJr <heliojr548@gmail.com>
2026-01-14 12:48:16 -0300

    Testes Automatizados: Utilizando padrao AAA para testes

--- Avancado/Testes-Automatizado/App.tsx
+++ Avancado/Testes-Automatizado/App.tsx
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+	const [counter, setCounter] = useState(0);
+
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>App contador</Text>
+			<Text style={styles.title}>App Contador</Text>
+
+			<View style={styles.counterArea}>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => setCounter(counter - 1)}
+				>
+					<Text style={styles.buttonText}>-</Text>
+				</TouchableOpacity>
+
+				<Text testID="counter" style={{ fontSize: 18 }}>
+					{counter}
+				</Text>
+
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => setCounter(counter + 1)}
+				>
+					<Text style={styles.buttonText}>+</Text>
+				</TouchableOpacity>
+			</View>
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -21,5 +44,21 @@ const styles = StyleSheet.create({
 		fontSize: 24,
 		fontWeight: 'bold',
 		marginTop: 14,
+	},
+	counterArea: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 14,
+		marginVertical: 14,
+	},
+	button: {
+		backgroundColor: '#1f76e7',
+		padding: 14,
+		borderRadius: 8,
+	},
+	buttonText: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		color: '#fff',
 	},
 });
--- Avancado/Testes-Automatizado/__test__/app/App.test.tsx
+++ Avancado/Testes-Automatizado/__test__/app/App.test.tsx
@@ -1,10 +1,51 @@
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import App from '../../App';
 
-it('Teste exemplo', () => {
+it('Test exemplo', () => {
+	const num1 = 10;
+	const num2 = 20;
+	const sum = num1 + num2;
+
 	expect(1).toBe(1);
+	expect(sum).toBe(30);
+	expect(sum).not.toBe(50);
+	expect(sum).toBeGreaterThan(20);
+	expect(sum).toBeLessThan(50);
 });
 
-it('Test render App component', () => {
-	render(<App />);
+describe('App Componente testes', () => {
+	it('Test render App component', () => {
+		const { getByText } = render(<App />);
+
+		expect(getByText('App Contador')).toBeTruthy();
+	});
+
+	/* 
+		padrão AAA (Arrange, Act, Assert) :
+		Arrange (preparar o ambiente, como mocks),
+		Act (executar a ação testada) 
+		e Assert (verificar o resultado esperado)
+	*/
+
+	it('should increase counter on press button', () => {
+		const { getByText, getByTestId } = render(<App />);
+
+		const button = getByText('+');
+		fireEvent.press(button);
+		fireEvent.press(button);
+
+		const counterText = getByTestId('counter');
+
+		expect(counterText.props.children).toBe(2);
+	});
+
+	it('should decrease counter on press button', () => {
+		const { getByText, getByTestId } = render(<App />);
+
+		const button = getByText('-');
+		fireEvent.press(button);
+
+		const counterText = getByTestId('counter');
+		expect(counterText.props.children).toBe(-1);
+	});
 });
