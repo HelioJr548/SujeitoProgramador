12b7557a40ef9e42ec026872be6d6719f045925f
HelioJr <heliojr548@gmail.com>
2026-01-14 15:05:55 -0300

    Testes Automatizados: Timer, act

--- Avancado/Testes-Automatizado/__test__/message/message.test.tsx
+++ Avancado/Testes-Automatizado/__test__/message/message.test.tsx
@@ -1,7 +1,11 @@
-import { fireEvent, render, screen } from '@testing-library/react-native';
+import { act, fireEvent, render, screen } from '@testing-library/react-native';
 import Message from '../../src/Message';
 
 describe('Component Message', () => {
+	// afterAll(() => {
+	// 	jest.clearAllTimers();
+	// });
+
 	it('should render text message', () => {
 		// const { getByTestId } = render(<Message />);
 		// expect(getByTestId('message').props.children).toBe('Aguardando...');
@@ -12,13 +16,17 @@ describe('Component Message', () => {
 	});
 
 	it('should change messasge on click button', async () => {
+		jest.useFakeTimers();
 		render(<Message />);
 
 		expect(screen.getByTestId('message').props.children).toBe(
 			'Aguardando...'
 		);
 
-		fireEvent.press(screen.getByText('Acessar'));
+		act(() => {
+			jest.runAllTimers();
+			fireEvent.press(screen.getByText('Acessar'));
+		});
 
 		// expect(screen.getByTestId('message').props.children).toBe(
 		// 	'Bem vindo Helio'
--- Avancado/Testes-Automatizado/src/Message.tsx
+++ Avancado/Testes-Automatizado/src/Message.tsx
@@ -4,16 +4,19 @@ import { Button, Text, View } from 'react-native';
 export default function Message() {
 	const [showMessage, setShowMessage] = useState(false);
 
+	function handleShowMessage() {
+		setTimeout(() => {
+			setShowMessage(!showMessage);
+		}, 1000);
+	}
+
 	return (
 		<View>
 			<Text testID="message">
 				{showMessage ? 'Bem vindo Helio' : 'Aguardando...'}
 			</Text>
 
-			<Button
-				title="Acessar"
-				onPress={() => setShowMessage(!showMessage)}
-			/>
+			<Button title="Acessar" onPress={handleShowMessage} />
 		</View>
 	);
 }
