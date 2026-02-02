e0b631608947a05b7119d55acbce55d975a9eaee
HelioJr <heliojr548@gmail.com>
2026-02-02 11:50:31 -0300

    Pagina de detalhes da viagem

--- Avancado/Exercicios/planejeiApp/src/screens/travel/detail/index.tsx
+++ Avancado/Exercicios/planejeiApp/src/screens/travel/detail/index.tsx
@@ -1,8 +1,20 @@
+import Index from '@/src/app';
 import colors from '@/src/constants/colors';
 import { TTravel } from '@/src/services/travel-services';
-import { Feather } from '@expo/vector-icons';
+import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { Link } from 'expo-router';
-import { Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
+import {
+	Platform,
+	Pressable,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 interface ITravelDetialScreenProps {
@@ -14,6 +26,17 @@ export function TravelDetailScreen({
 	loading,
 	travel,
 }: ITravelDetialScreenProps) {
+	if (loading || !travel) return <Index />;
+
+	const formattedStartDate = format(
+		parseISO(travel.start_date),
+		'dd MMMM yyyy',
+		{ locale: ptBR },
+	);
+	const formattedEndDate = format(parseISO(travel.end_date), 'dd MMMM yyyy', {
+		locale: ptBR,
+	});
+
 	return (
 		<SafeAreaView style={styles.safeArea}>
 			<StatusBar
@@ -32,12 +55,164 @@ export function TravelDetailScreen({
 
 					<Text style={styles.title}>Planejei</Text>
 				</View>
+
+				<View>
+					<Text style={[styles.heading, { marginTop: 14 }]}>
+						Detalhes da sua viagem para
+					</Text>
+					<Text
+						style={[
+							styles.heading,
+							{ fontWeight: '600', marginBottom: 14 },
+						]}
+					>
+						{travel?.city}
+					</Text>
+				</View>
+
+				<ScrollView showsVerticalScrollIndicator={false}>
+					<View style={styles.infoContainer}>
+						<View style={styles.infoRow}>
+							<MaterialCommunityIcons
+								name="airplane-takeoff"
+								size={24}
+								color={colors.white}
+							></MaterialCommunityIcons>
+							<Text style={styles.infoDate}>
+								{formattedStartDate}{' '}
+							</Text>
+						</View>
+						<View style={styles.infoRow}>
+							<MaterialCommunityIcons
+								name="airplane-landing"
+								size={24}
+								color={colors.white}
+							></MaterialCommunityIcons>
+							<Text style={styles.infoDate}>
+								{formattedEndDate}{' '}
+							</Text>
+						</View>
+					</View>
+
+					<View style={styles.cardContainer}>
+						<Text style={styles.cardLabel}>Cidade:</Text>
+						<Text style={styles.cardValue}>{travel?.city}</Text>
+
+						<Text style={styles.cardLabel}>Hotel:</Text>
+						<Text style={styles.cardValue}>
+							{travel?.hotel_address}
+						</Text>
+
+						<Pressable style={styles.deleteButton}>
+							<Text style={styles.deleteButtonText}>
+								Excluir viagem
+							</Text>
+						</Pressable>
+					</View>
+
+					<Text style={styles.sectionTitle}>Lembretes</Text>
+
+					<View style={styles.reminderInputContainer}>
+						<TextInput
+							placeholder="Digite um lembrete"
+							placeholderTextColor={colors.gray100}
+							style={styles.reminderInput}
+						/>
+						<Pressable style={styles.addButton}>
+							<Feather
+								name="plus"
+								size={24}
+								color={colors.white}
+							/>
+						</Pressable>
+					</View>
+
+					<View style={styles.spacingVertical}>
+						<View style={styles.reminderItem}>
+							<Text style={styles.reminderText}>
+								Lembrar de pegar a chave do AP
+							</Text>
+
+							<Pressable>
+								<Feather
+									name="trash"
+									size={20}
+									color={colors.red}
+								/>
+							</Pressable>
+						</View>
+					</View>
+				</ScrollView>
 			</View>
 		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create({
+	reminderText: { flex: 1, color: colors.white },
+	reminderItem: {
+		backgroundColor: colors.gray200,
+		padding: 10,
+		borderRadius: 8,
+		marginBottom: 14,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+	spacingVertical: { marginBottom: 14 },
+	addButton: { backgroundColor: colors.orange, padding: 8, borderRadius: 4 },
+	reminderInput: {
+		flex: 1,
+		color: colors.white,
+		paddingVertical: 8,
+	},
+	reminderInputContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		backgroundColor: colors.gray200,
+		borderRadius: 8,
+		padding: 8,
+		marginBottom: 14,
+	},
+	sectionTitle: {
+		color: colors.white,
+		fontSize: 18,
+		marginBottom: 8,
+		fontWeight: '600',
+	},
+	deleteButtonText: {
+		color: colors.red,
+		fontWeight: '500',
+	},
+	deleteButton: {
+		backgroundColor: 'transparent',
+		padding: 8,
+		borderRadius: 4,
+		borderColor: colors.red,
+		borderWidth: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	cardValue: {
+		color: colors.black,
+		fontSize: 16,
+		fontWeight: '500',
+		marginBottom: 14,
+	},
+	cardLabel: { color: colors.zinc },
+	cardContainer: {
+		backgroundColor: colors.white,
+		padding: 16,
+		borderRadius: 8,
+		marginVertical: 16,
+	},
+	infoDate: { color: colors.white, fontSize: 16 },
+	infoRow: { flexDirection: 'row', gap: 8, alignItems: 'center' },
+	infoContainer: { gap: 8 },
+	heading: {
+		color: colors.white,
+		fontSize: 24,
+	},
 	safeArea: {
 		flex: 1,
 		backgroundColor: colors.zinc,
