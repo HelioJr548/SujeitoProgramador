87937ac64ec8ed74e4cd5d439bada0c5381b351b
HelioJr <heliojr548@gmail.com>
2026-03-25 15:57:48 -0300

    MovieFlix TabBar

---
+++ React-Native/Avancado/Exercicios/movieApp/app/onboarding.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function onboarding() {
+	return (
+		<View>
+			<Text> textInComponent </Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
