5da5f179477d2d9a409011d0d79bf8290e335c5a
HelioJr <heliojr548@gmail.com>
2026-06-07 17:44:31 -0300

    Eventos

--- ReactJS/primeiro-app/src/App.js
+++ ReactJS/primeiro-app/src/App.js
@@ -1,29 +1,11 @@
 import { Component } from 'react';
+import Membro from './components/Membro';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			hora: '00:00:00',
-		};
-	}
-
-	componentDidMount() {
-		setInterval(() => {
-			this.setState({
-				hora: new Date().toLocaleTimeString('pt-BR'),
-			});
-		}, 1000);
-	}
-
-	componentDidUpdate() {
-		console.log('Atualizou');
-	}
-
 	render() {
 		return (
 			<div>
-				<h1>Contador {this.state.hora}</h1>
+				<Membro nome="Visitante" />
 			</div>
 		);
 	}
