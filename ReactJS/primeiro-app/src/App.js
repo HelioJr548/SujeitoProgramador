1122d2762841112cb65b0435721f2ec4d0c2246a
HelioJr <heliojr548@gmail.com>
2026-06-07 19:46:20 -0300

    Manipulando Listas

--- ReactJS/primeiro-app/src/App.js
+++ ReactJS/primeiro-app/src/App.js
@@ -1,38 +1,32 @@
 import { Component } from 'react';
+import Feed from './components/Feed';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			status: false,
+			feed: [
+				{ id: 1, username: 'Helio', curtidas: 10, comentarios: 5 },
+				{ id: 2, username: 'Joao', curtidas: 102, comentarios: 25 },
+				{ id: 3, username: 'Lucas', curtidas: 20, comentarios: 14 },
+				{ id: 4, username: 'Amanda', curtidas: 0, comentarios: 0 },
+			],
 		};
-
-		this.entrar = this.entrar.bind(this);
-		this.sair = this.sair.bind(this);
-	}
-
-	entrar() {
-		this.setState({ status: true });
-	}
-
-	sair() {
-		this.setState({ status: false });
 	}
 
 	render() {
 		return (
 			<div>
-				{this.state.status ? (
-					<div>
-						<h2>Bem-vindo ao sistema</h2>
-						<button onClick={this.sair}>Sair</button>
-					</div>
-				) : (
-					<div>
-						<h2>Olá visitante, faça o login!</h2>
-						<button onClick={this.entrar}>entrar</button>
-					</div>
-				)}
+				{this.state.feed.map((item) => {
+					return (
+						<Feed
+							key={item.id}
+							username={item.username}
+							curtidas={item.curtidas}
+							comentarios={item.comentarios}
+						/>
+					);
+				})}
 			</div>
 		);
 	}
